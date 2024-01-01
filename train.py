c9047fd035c568dd3562bfb20830214da511a87a
Kiet0712 <kiet.dangvutuan0712@hcmut.edu.vn>
2024-01-01 21:02:19 +0700

    first commit

---
+++ train.py
@@ -0,0 +1,173 @@
+from torch.utils.data import DataLoader
+import torch
+from src.MHCRFBDMPAUnet3D import MHCRFBDMPAUnet3D as MHCRFBDMPAUnet3D
+from dataset.dataset import BRATS
+from utils.loss import MHLoss_1
+import torch.nn.functional as F
+import numpy as np
+from scipy.spatial.distance import directed_hausdorff
+model_choice = {
+    'MHCRFBDMPAUnet3D':MHCRFBDMPAUnet3D
+}
+
+csv_dir = ''
+root_dir = ''
+data_train = BRATS(
+    csv_dir=csv_dir,
+    mode='train',
+    root_dir=root_dir
+)
+data_val = BRATS(
+    csv_dir=csv_dir,
+    mode='val',
+    root_dir=root_dir
+)
+train_dataloader = DataLoader(data_train,1,True)
+val_dataloader = DataLoader(data_val,1,True)
+model = model_choice['MHCRFBDMPAUnet3D'](4,3)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+def pad_batch1_to_compatible_size(batch):
+    shape = batch.shape
+    zyx = list(shape[-3:])
+    for i, dim in enumerate(zyx):
+        max_stride = 16
+        if dim % max_stride != 0:
+            # Make it divisible by 16
+            zyx[i] = ((dim // max_stride) + 1) * max_stride
+    zmax, ymax, xmax = zyx
+    zpad, ypad, xpad = zmax - batch.size(2), ymax - batch.size(3), xmax - batch.size(4)
+    assert all(pad >= 0 for pad in (zpad, ypad, xpad)), "Negative padding value error !!"
+    pads = (0, xpad, 0, ypad, 0, zpad)
+    batch = F.pad(batch, pads)
+    return batch, (zpad, ypad, xpad)
+def calculate_metrics(predict,gt):
+    labels = ["ET", "TC", "WT"]
+    results = []
+    for i, label in enumerate(labels):
+        if np.sum(gt[i])==0:
+            print('Remove sample')
+            print('Non '  + label)
+            return []
+        preds_coords = np.argwhere(predict[i])
+        targets_coords = np.argwhere(gt[i])
+        haussdorf_dist = directed_hausdorff(preds_coords, targets_coords)[0]
+        tp = np.sum((predict[i]==1)&(gt[i]==1))
+        tn = np.sum((predict[i]==0)&(gt[i]==0))
+        fp = np.sum((predict[i]==1)&(gt[i]==0))
+        fn = np.sum((predict[i]==0)&(gt[i]==1))
+        sens = tp/(tp+fn)
+        spec = tn/(tn+fp)
+        dice = 2*tp/(2*tp+fp+fn)
+        results.append([
+            haussdorf_dist,
+            sens,
+            spec,
+            dice
+        ])
+    return results
+def print_validation_result(et, tc, wt, name_metrics=["HDis      ", "Sens      ", "Spec      ", "Dice"]):
+  """
+  Prints the Validation result with corresponding name metrics for three numpy arrays.
+
+  Args:
+    et: A numpy array containing the first row of data.
+    tc: A numpy array containing the second row of data.
+    wt: A numpy array containing the third row of data.
+    name_metrics: A list of strings containing the corresponding name metrics for each column.
+
+  Returns:
+    None
+  """
+  # Check if the number of columns and name metrics match
+  if len(et) != len(tc) != len(wt) != len(name_metrics):
+    raise ValueError("The number of columns and name metrics must be equal.")
+
+  # Print the Validation result with row names
+  print("Validation result:")
+  print("     ", *name_metrics)  # Print header with metrics names
+  print("ET:  ", *et)  # Print ET row with values
+  print("TC:  ", *tc)  # Print TC row with values
+  print("WT:  ", *wt)  # Print WT row with values
+def validation(val_dataloader,model):
+    result_metrics = []
+    for i,data in enumerate(val_dataloader):
+        inputs = data['img']
+        crop_idx = data['crop_indices']
+        inputs,pad = pad_batch1_to_compatible_size(inputs)
+        inputs = inputs.to(device)
+        gt_seg_volume = data['label']
+        with torch.inference_mode():
+            outputs = model(inputs)
+            predict_segment_volume = outputs['segment_volume']
+        maxz,maxy,maxx = predict_segment_volume.size(2)-pad[0],predict_segment_volume.size(3)-pad[1],predict_segment_volume.size(4)-pad[2]
+        predict_segment_volume = predict_segment_volume[:,:,0:maxz,0:maxy,0:maxx].cpu().numpy()
+        predict_seg_volume = np.zeros(gt_seg_volume.shape)
+        predict_seg_volume[:,:,slice(*crop_idx[0]),slice(*crop_idx[1]),slice(*crop_idx[2])] = predict_segment_volume
+        segs = (predict_seg_volume[0]>0.5).astype('float32')
+        patient_metrics_result = calculate_metrics(segs,gt_seg_volume[0].numpy()) 
+        if len(patient_metrics_result)!=0:
+            result_metrics.append(patient_metrics_result)
+    result_metrics = np.array(result_metrics)
+    mean_metrics_results = np.mean(result_metrics,axis=0)
+    print('Validation result:')
+    print_validation_result(mean_metrics_results[0],mean_metrics_results[1],mean_metrics_results[2])
+def train(train_dataloader,model,loss_func,optim,epochs,save_each_epoch,checkpoint_save_path):
+    for epoch in range(epochs):
+        running_loss = {}
+        for i,data in enumerate(train_dataloader):
+            inputs = data['img'].to(device)
+            label = data['label'].to(device)
+            optim.zero_grad()
+            outputs = model(inputs)
+            loss_cal = loss_func(outputs,inputs,label)
+            loss_cal['loss'].backward()
+            optim.step()
+            for key in loss_cal:
+                if i==0:
+                    running_loss[key]=loss_cal[key].item()
+                else:
+                    running_loss[key]+=loss_cal[key].item()
+            if i%100==0 and i!=0:
+                print('Epoch ' + str(epoch+1) + ', iter ' + str(i+1) + ':')
+                for key in running_loss:
+                    print(key + ' = ' + str(running_loss[key]/100))
+                    running_loss[key]=0
+        if epoch%save_each_epoch==0:
+            print('================================VALIDATION ' + str(epoch+1)+'================================')
+            validation(val_dataloader,model)
+            torch.save(
+                {
+                    'model_state_dict':model.state_dict(),
+                    'optim_state_dict':optim.state_dict()
+                },checkpoint_save_path
+            )
+optim = torch.optim.Adam(model.parameters(),lr=1e-4)
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optim,0.95)
+loss_func = MHLoss_1(
+    {
+        'segment_volume_loss':5,
+            'reconstruct_volume_loss':2,
+            'class_1_foreground_loss':2,
+            'class_2_foreground_loss':2,
+            'class_4_foreground_loss':2,
+            'class_1_background_loss':2,
+            'class_2_background_loss':2,
+            'class_4_background_loss':2,
+            'reconstruct_guide_loss':0.75,
+            'class_1_background_guide_loss':0.75,
+            'class_1_foreground_guide_loss':0.75,
+            'class_2_background_guide_loss':0.75,
+            'class_2_foreground_guide_loss':0.75,
+            'class_4_background_guide_loss':0.75,
+            'class_4_foreground_guide_loss':0.75
+    }
+)
+train(
+    train_dataloader,
+    model,
+    loss_func,
+    optim,
+    4,
+    'CHECKPOINT.pth'
+)
---
+++ utils/loss.py
@@ -0,0 +1,210 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BCELoss(nn.Module):
+    def __init__(self):
+        super(BCELoss, self).__init__()
+        self.bceloss = nn.BCELoss()
+
+    def forward(self, pred, target):
+        sizep = pred.size(0)
+        sizet = target.size(0)
+        pred_flat = pred.view(sizep, -1)
+        target_flat = target.view(sizet, -1)
+
+        loss = self.bceloss(pred_flat, target_flat)
+
+        return loss
+
+#Dice loss
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, pred, target):
+        smooth = 1
+
+        size = target.size(0)
+
+        pred_flat = pred.view(size, -1)
+        target_flat = target.view(size, -1)
+
+        intersection = pred_flat * target_flat
+        dice_score = (2 * intersection.sum(1) + smooth)/ (pred_flat.sum(1) + target_flat.sum(1) + smooth)
+        dice_loss = 1 - dice_score.sum()/size
+
+        return dice_loss
+class LogCoshDiceLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.dice = DiceLoss()
+    def forward(self,pred,target):
+        loss = self.dice(pred,target)
+        return torch.math.log((torch.exp(loss)+torch.exp(-loss))/2)
+# BCE + Dice  loss
+class BceDiceLoss(nn.Module):
+    def __init__(self):
+        super(BceDiceLoss, self).__init__()
+        self.bce = BCELoss()
+        self.dice = DiceLoss()
+
+    def forward(self, pred, target):
+        bceloss = self.bce(pred,target)
+        diceloss = self.dice(pred,target)
+
+        loss = diceloss + bceloss
+
+        return loss
+class BceLogCoshDiceLoss(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.bce = BCELoss()
+        self.logcoshdice = LogCoshDiceLoss()
+
+    def forward(self, pred, target):
+        bceloss = self.bce(pred,target)
+        logcoshdice = self.logcoshdice(pred,target)
+
+        loss = logcoshdice + bceloss
+
+        return loss    
+class MHLoss_1(nn.Module):
+    def __init__(self,lamda_list):
+        super().__init__()
+        self.lamda_list = lamda_list
+        self.dicebce = BceDiceLoss()
+    def forward(self,data,volume,gt_volume):
+        segment_volume = data['segment_volume']
+        reconstruct_volume = data['reconstruct_volume']
+        class_1_foreground = data['class_1_foreground']
+        class_2_foreground = data['class_2_foreground']
+        class_4_foreground = data['class_4_foreground']
+        class_1_background = data['class_1_background']
+        class_2_background = data['class_2_background']
+        class_4_background = data['class_4_background']
+
+
+        #calculate_main_loss
+        segment_volume_loss = self.dicebce(segment_volume,gt_volume)
+        reconstruct_volume_loss = F.l1_loss(reconstruct_volume,volume)
+        class_1_foreground_loss = F.l1_loss(class_1_foreground,volume*gt_volume[:,1:2,:,:,:])
+        class_2_foreground_loss = F.l1_loss(class_2_foreground,volume*gt_volume[:,2:,:,:,:])
+        class_4_foreground_loss = F.l1_loss(class_4_foreground,volume*gt_volume[:,0:1,:,:,:])
+        class_1_background_loss = F.l1_loss(class_1_background,volume*(1-gt_volume[:,1:2,:,:,:]))
+        class_2_background_loss = F.l1_loss(class_2_background,volume*(1-gt_volume[:,2:,:,:,:]))
+        class_4_background_loss = F.l1_loss(class_4_background,volume*(1-gt_volume[:,0:1,:,:,:]))
+        #calculate_guide_loss
+        reconstruct_guide_loss = F.l1_loss(reconstruct_volume,class_1_background+class_1_foreground)+\
+                                 F.l1_loss(reconstruct_volume,class_2_background+class_2_foreground)+\
+                                 F.l1_loss(reconstruct_volume,class_4_background+class_4_foreground)
+        class_1_background_guide_loss = F.l1_loss(class_1_background,reconstruct_volume*(1-segment_volume[:,1:2,:,:,:]))
+        class_1_foreground_guide_loss = F.l1_loss(class_1_foreground,reconstruct_volume*segment_volume[:,1:2,:,:,:])
+        class_2_background_guide_loss = F.l1_loss(class_2_background,reconstruct_volume*(1-segment_volume[:,2:,:,:,:]))
+        class_2_foreground_guide_loss = F.l1_loss(class_2_foreground,reconstruct_volume*segment_volume[:,2:,:,:,:])
+        class_4_background_guide_loss = F.l1_loss(class_4_background,reconstruct_volume*(1-segment_volume[:,0:1,:,:,:]))
+        class_4_foreground_guide_loss = F.l1_loss(class_4_foreground,reconstruct_volume*segment_volume[:,0:1,:,:,:])
+
+        loss = self.lamda_list['segment_volume_loss']*segment_volume_loss+\
+               self.lamda_list['reconstruct_volume_loss']*reconstruct_volume_loss+\
+               self.lamda_list['class_1_foreground_loss']*class_1_foreground_loss+\
+               self.lamda_list['class_2_foreground_loss']*class_2_foreground_loss+\
+               self.lamda_list['class_4_foreground_loss']*class_4_foreground_loss+\
+               self.lamda_list['class_1_background_loss']*class_1_background_loss+\
+               self.lamda_list['class_2_background_loss']*class_2_background_loss+\
+               self.lamda_list['class_4_background_loss']*class_4_background_loss+\
+               self.lamda_list['reconstruct_guide_loss']*reconstruct_guide_loss+\
+               self.lamda_list['class_1_background_guide_loss']*class_1_background_guide_loss+\
+               self.lamda_list['class_1_foreground_guide_loss']*class_1_foreground_guide_loss+\
+               self.lamda_list['class_2_background_guide_loss']*class_2_background_guide_loss+\
+               self.lamda_list['class_2_foreground_guide_loss']*class_2_foreground_guide_loss+\
+               self.lamda_list['class_4_background_guide_loss']*class_4_background_guide_loss+\
+               self.lamda_list['class_4_foreground_guide_loss']*class_4_foreground_guide_loss
+        return {
+            'loss': loss,
+            'segment_volume_loss':segment_volume_loss,
+            'reconstruct_volume_loss':reconstruct_volume_loss,
+            'class_1_foreground_loss':class_1_foreground_loss,
+            'class_2_foreground_loss':class_2_foreground_loss,
+            'class_4_foreground_loss':class_4_foreground_loss,
+            'class_1_background_loss':class_1_background_loss,
+            'class_2_background_loss':class_2_background_loss,
+            'class_4_background_loss':class_4_background_loss,
+            'reconstruct_guide_loss':reconstruct_guide_loss,
+            'class_1_background_guide_loss':class_1_background_guide_loss,
+            'class_1_foreground_guide_loss':class_1_foreground_guide_loss,
+            'class_2_background_guide_loss':class_2_background_guide_loss,
+            'class_2_foreground_guide_loss':class_2_foreground_guide_loss,
+            'class_4_background_guide_loss':class_4_background_guide_loss,
+            'class_4_foreground_guide_loss':class_4_foreground_guide_loss
+        }
+class MHLoss_2(nn.Module):
+    def __init__(self,lamda_list):
+        super().__init__()
+        self.lamda_list = lamda_list
+        self.bcelogcoshdice = BceLogCoshDiceLoss()
+    def forward(self,data,volume,gt_volume):
+        segment_volume = data['segment_volume']
+        reconstruct_volume = data['reconstruct_volume']
+        class_1_foreground = data['class_1_foreground']
+        class_2_foreground = data['class_2_foreground']
+        class_4_foreground = data['class_4_foreground']
+        class_1_background = data['class_1_background']
+        class_2_background = data['class_2_background']
+        class_4_background = data['class_4_background']
+
+
+        #calculate_main_loss
+        segment_volume_loss = self.bcelogcoshdice(segment_volume,gt_volume)
+        reconstruct_volume_loss = F.l1_loss(reconstruct_volume,volume)
+        class_1_foreground_loss = F.l1_loss(class_1_foreground,volume*gt_volume[:,1:2,:,:,:])
+        class_2_foreground_loss = F.l1_loss(class_2_foreground,volume*gt_volume[:,2:,:,:,:])
+        class_4_foreground_loss = F.l1_loss(class_4_foreground,volume*gt_volume[:,0:1,:,:,:])
+        class_1_background_loss = F.l1_loss(class_1_background,volume*(1-gt_volume[:,1:2,:,:,:]))
+        class_2_background_loss = F.l1_loss(class_2_background,volume*(1-gt_volume[:,2:,:,:,:]))
+        class_4_background_loss = F.l1_loss(class_4_background,volume*(1-gt_volume[:,0:1,:,:,:]))
+        #calculate_guide_loss
+        reconstruct_guide_loss = F.l1_loss(reconstruct_volume,class_1_background+class_1_foreground)+\
+                                 F.l1_loss(reconstruct_volume,class_2_background+class_2_foreground)+\
+                                 F.l1_loss(reconstruct_volume,class_4_background+class_4_foreground)
+        class_1_background_guide_loss = F.l1_loss(class_1_background,reconstruct_volume*(1-segment_volume[:,1:2,:,:,:]))
+        class_1_foreground_guide_loss = F.l1_loss(class_1_foreground,reconstruct_volume*segment_volume[:,1:2,:,:,:])
+        class_2_background_guide_loss = F.l1_loss(class_2_background,reconstruct_volume*(1-segment_volume[:,2:,:,:,:]))
+        class_2_foreground_guide_loss = F.l1_loss(class_2_foreground,reconstruct_volume*segment_volume[:,2:,:,:,:])
+        class_4_background_guide_loss = F.l1_loss(class_4_background,reconstruct_volume*(1-segment_volume[:,0:1,:,:,:]))
+        class_4_foreground_guide_loss = F.l1_loss(class_4_foreground,reconstruct_volume*segment_volume[:,0:1,:,:,:])
+
+        loss = self.lamda_list['segment_volume_loss']*segment_volume_loss+\
+               self.lamda_list['reconstruct_volume_loss']*reconstruct_volume_loss+\
+               self.lamda_list['class_1_foreground_loss']*class_1_foreground_loss+\
+               self.lamda_list['class_2_foreground_loss']*class_2_foreground_loss+\
+               self.lamda_list['class_4_foreground_loss']*class_4_foreground_loss+\
+               self.lamda_list['class_1_background_loss']*class_1_background_loss+\
+               self.lamda_list['class_2_background_loss']*class_2_background_loss+\
+               self.lamda_list['class_4_background_loss']*class_4_background_loss+\
+               self.lamda_list['reconstruct_guide_loss']*reconstruct_guide_loss+\
+               self.lamda_list['class_1_background_guide_loss']*class_1_background_guide_loss+\
+               self.lamda_list['class_1_foreground_guide_loss']*class_1_foreground_guide_loss+\
+               self.lamda_list['class_2_background_guide_loss']*class_2_background_guide_loss+\
+               self.lamda_list['class_2_foreground_guide_loss']*class_2_foreground_guide_loss+\
+               self.lamda_list['class_4_background_guide_loss']*class_4_background_guide_loss+\
+               self.lamda_list['class_4_foreground_guide_loss']*class_4_foreground_guide_loss
+        return {
+            'loss': loss,
+            'segment_volume_loss':segment_volume_loss,
+            'reconstruct_volume_loss':reconstruct_volume_loss,
+            'class_1_foreground_loss':class_1_foreground_loss,
+            'class_2_foreground_loss':class_2_foreground_loss,
+            'class_4_foreground_loss':class_4_foreground_loss,
+            'class_1_background_loss':class_1_background_loss,
+            'class_2_background_loss':class_2_background_loss,
+            'class_4_background_loss':class_4_background_loss,
+            'reconstruct_guide_loss':reconstruct_guide_loss,
+            'class_1_background_guide_loss':class_1_background_guide_loss,
+            'class_1_foreground_guide_loss':class_1_foreground_guide_loss,
+            'class_2_background_guide_loss':class_2_background_guide_loss,
+            'class_2_foreground_guide_loss':class_2_foreground_guide_loss,
+            'class_4_background_guide_loss':class_4_background_guide_loss,
+            'class_4_foreground_guide_loss':class_4_foreground_guide_loss
+        }
