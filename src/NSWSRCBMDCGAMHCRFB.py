4c3ae05c9916dc708832c48bb60ca2f0ee1d714e
Kiet0712 <kiet.dangvutuan0712@hcmut.edu.vn>
2024-01-12 15:15:43 +0700

    update NSW Dynamic Conv

--- src/NSWSRCBMDCGAMHCRFB.py
+++ src/NSWSRCBMDCGAMHCRFB.py
@@ -77,16 +77,28 @@ class OutConv(nn.Module):
         self.class_2_segment_conv = MDynamic_conv3d(in_channels,out_channels//3,1)
         self.class_4_segment_conv = MDynamic_conv3d(in_channels,out_channels//3,1)
         self.class_1_variant_guide = nn.Sequential(
-            nn.Conv3d(n_channels*2,in_channels,1),
-            nn.BatchNorm3d(in_channels)
+            nn.Conv3d(n_channels*2,in_channels,1,bias=False),
+            nn.BatchNorm3d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(n_channels*2,in_channels,1,bias=False),
+            nn.BatchNorm3d(in_channels),
+            nn.ReLU(inplace=True)
         )
         self.class_2_variant_guide = nn.Sequential(
-            nn.Conv3d(n_channels*2,in_channels,1),
-            nn.BatchNorm3d(in_channels)
+            nn.Conv3d(n_channels*2,in_channels,1,bias=False),
+            nn.BatchNorm3d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(n_channels*2,in_channels,1,bias=False),
+            nn.BatchNorm3d(in_channels),
+            nn.ReLU(inplace=True)
         )
         self.class_4_variant_guide = nn.Sequential(
-            nn.Conv3d(n_channels*2,in_channels,1),
-            nn.BatchNorm3d(in_channels)
+            nn.Conv3d(n_channels*2,in_channels,1,bias=False),
+            nn.BatchNorm3d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(n_channels*2,in_channels,1,bias=False),
+            nn.BatchNorm3d(in_channels),
+            nn.ReLU(inplace=True)
         )
         self.reconstruct_variant_guide = DoubleConv(n_channels,n_channels)
     def forward(self, x):
