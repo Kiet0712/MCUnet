d6f4cbfcd9e575b66b0f620fd593c0b4ee68728e
Kiet0712 <kiet.dangvutuan0712@hcmut.edu.vn>
2024-03-15 16:26:47 +0700

    more flexible implement of selfguideoutput

--- src/outconv.py
+++ src/outconv.py
@@ -11,6 +11,8 @@ class OutConv(nn.Module):
         self.MULTIHEAD_OUTPUT = cfg.MODEL.MULTIHEAD_OUTPUT
         self.OUTPUT_COORDCONV = cfg.MODEL.OUTPUT_COORDCONV
         self.SELF_GUIDE_OUTPUT = cfg.MODEL.SELF_GUIDE_OUTPUT
+        self.n_class = out_channels
+        self.n_channels = n_channels
         if cfg.MODEL.NORM=='IN3d':
             norm = nn.InstanceNorm3d
         elif cfg.MODEL.NORM == 'BN3d':
@@ -41,21 +43,18 @@ class OutConv(nn.Module):
                     conv(in_channels+n_channels,out_channels*n_channels*2,kernel_size=1),
                     nn.Sigmoid() if cfg.MODEL.SELF_GUIDE_OUTPUT_SIGMOID else nn.Identity()
                 )
-                self.class_1_segment_conv = nn.Sequential(
-                    conv(in_channels+n_channels*2,out_channels//3,kernel_size=1),
-                    nn.Sigmoid()
-                )
-                self.class_2_segment_conv = nn.Sequential(
-                    conv(in_channels+n_channels*2,out_channels//3,kernel_size=1),
-                    nn.Sigmoid()
-                )
-                self.class_4_segment_conv = nn.Sequential(
-                    conv(in_channels+n_channels*2,out_channels//3,kernel_size=1),
-                    nn.Sigmoid()
-                )
-                self.attention_class_1 = Attention_block(cfg,in_channels,n_channels*2,in_channels//4)
-                self.attention_class_2 = Attention_block(cfg,in_channels,n_channels*2,in_channels//4)
-                self.attention_class_4 = Attention_block(cfg,in_channels,n_channels*2,in_channels//4)
+                self.class_segment_conv = nn.ModuleList()
+                self.attention = nn.ModuleList()
+                for _ in range(self.n_class):
+                    self.class_segment_conv.append(
+                        nn.Sequential(
+                            conv(in_channels+n_channels*2,out_channels//3,kernel_size=1),
+                            nn.Sigmoid()
+                        )
+                    )
+                    self.attention.append(
+                        Attention_block(cfg,in_channels,n_channels*2,in_channels//4)
+                    )
                 self.attention_reconstruct = Attention_block(cfg,in_channels,n_channels,in_channels//4)
             else:
                 self.reconstruct_volume_conv = nn.Sequential(
@@ -80,37 +79,35 @@ class OutConv(nn.Module):
             if self.SELF_GUIDE_OUTPUT:
                 reconstruct_volume = self.reconstruct_volume_conv(x)
                 mask_head = self.mask_head(torch.cat([x,self.attention_reconstruct(x,reconstruct_volume)],dim=1))
-                class_1_guide = torch.cat([mask_head[:,0:4,:,:,:],mask_head[:,12:16,:,:,:]],dim=1)
-                class_2_guide = torch.cat([mask_head[:,4:8,:,:,:],mask_head[:,16:20,:,:,:]],dim=1)
-                class_4_guide = torch.cat([mask_head[:,8:12,:,:,:],mask_head[:,20:,:,:,:]],dim=1)
-                class_1_segment_vol = self.class_1_segment_conv(torch.cat([x,self.attention_class_1(x,class_1_guide)],dim=1))
-                class_2_segment_vol = self.class_2_segment_conv(torch.cat([x,self.attention_class_2(x,class_2_guide)],dim=1))
-                class_4_segment_vol = self.class_4_segment_conv(torch.cat([x,self.attention_class_4(x,class_4_guide)],dim=1))
-                segment_volume = torch.cat([class_4_segment_vol,class_1_segment_vol,class_2_segment_vol],dim=1)
-                return {
+                segment_volume = []
+                for i in range(self.n_class):
+                    class_i_guide = torch.cat([mask_head[:,i*self.n_channels:(i+1)*self.n_channels],mask_head[:,(i*self.n_channels+self.n_channels*self.n_class):((i+1)*self.n_channels+self.n_channels*self.n_class)]],dim=1)
+                    segment_volume.append(
+                        self.class_segment_conv[i](torch.cat([x,self.attention[i](x,class_i_guide)],dim=1))
+                    )
+                segment_volume = torch.cat(segment_volume,dim=1)
+                result_dict = {
                     'segment_volume':segment_volume,
-                    'reconstruct_volume':reconstruct_volume,
-                    'class_1_foreground':mask_head[:,0:4,:,:,:],
-                    'class_2_foreground':mask_head[:,4:8,:,:,:],
-                    'class_4_foreground':mask_head[:,8:12,:,:,:],
-                    'class_1_background':mask_head[:,12:16,:,:,:],
-                    'class_2_background':mask_head[:,16:20,:,:,:],
-                    'class_4_background':mask_head[:,20:,:,:,:]
+                    'reconstruct_volume':reconstruct_volume
                 }
+                for i in range(self.n_class):
+                    str_class = 'class_' + str(i+1) + '_'
+                    result_dict[str_class+'foreground'] = mask_head[:,i*self.n_channels:(i+1)*self.n_channels]
+                    result_dict[str_class+'background'] = mask_head[:,(i*self.n_channels+self.n_channels*self.n_class):((i+1)*self.n_channels+self.n_channels*self.n_class)]
+                return result_dict
             else:
                 reconstruct_volume = self.reconstruct_volume_conv(x)
                 mask_head = self.mask_head(x)
                 segment_volume = self.segment_conv(x)
-                return {
+                result_dict = {
                     'segment_volume':segment_volume,
-                    'reconstruct_volume':reconstruct_volume,
-                    'class_1_foreground':mask_head[:,0:4,:,:,:],
-                    'class_2_foreground':mask_head[:,4:8,:,:,:],
-                    'class_4_foreground':mask_head[:,8:12,:,:,:],
-                    'class_1_background':mask_head[:,12:16,:,:,:],
-                    'class_2_background':mask_head[:,16:20,:,:,:],
-                    'class_4_background':mask_head[:,20:,:,:,:]
+                    'reconstruct_volume':reconstruct_volume
                 }
+                for i in range(self.n_class):
+                    str_class = 'class_' + str(i+1) + '_'
+                    result_dict[str_class+'foreground'] = mask_head[:,i*self.n_channels:(i+1)*self.n_channels]
+                    result_dict[str_class+'background'] = mask_head[:,(i*self.n_channels+self.n_channels*self.n_class):((i+1)*self.n_channels+self.n_channels*self.n_class)]
+                return result_dict
         else:
             return self.output_conv(x)
             
